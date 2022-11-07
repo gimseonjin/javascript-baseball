7b52734d8fbad1d5ab4a40578a00131a2c8a54fd
gimseonjin616 <66009926+gimseonjin@users.noreply.github.com>
2022-11-07 17:26:28 +0900

    refactoring : Refactoring Restart Logic

--- src/App.js
+++ src/App.js
@@ -61,15 +61,19 @@ class App {
   checkAnswer(strike){
     if(strike == 3){
       MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
-      MissionUtils.Console.readLine('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.', (answer) => {
-        this.isRestart(answer)
-      })
+      this.isRestart()
     }else{
       this.startProgress()
     }
   }
 
-  isRestart(answer){
+  isRestart(){
+    MissionUtils.Console.readLine('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.', (answer) => {
+      this.restart(answer)
+    })
+  }
+
+  restart(answer){
     if(answer == 2) 
       throw Error()
     this.play()
